6ba2484a35ded20c3d8644420f45e1d16a837c0c
madoka <madokamiws@163.com>
2022-01-25 19:08:06 +0800

    内容完善

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,12 +19,21 @@ public class GameManager : MonoBehaviour
             },
             new ScriptData()
             {
-                loadType =2,name="Test",dialogueContent = "222222222222222"
+                loadType =2,name="Test",dialogueContent = "222222222222222",characterPos=2
             },
-                        new ScriptData()
+            new ScriptData()
             {
-                loadType =3,name="Test",dialogueContent = "333333333333333"
+                loadType =3,name="Test",dialogueContent = "333333333333333",characterPos=1,ifRotate = true
             },
+            new ScriptData()
+            {
+                loadType =4,name="Test",dialogueContent = "4444444444444444444",characterPos=3
+            },
+            new ScriptData()
+            {
+                loadType =4,name="Test",dialogueContent = "666666666666666",characterPos=2
+            },
+
         };
         scriptIndex = 0;
         HandleDate();
@@ -54,12 +63,14 @@ public class GameManager : MonoBehaviour
             ShowCharacter(scriptDatas[scriptIndex].name);
             //更新对话框
             UpdateTalkLineText(scriptDatas[scriptIndex].dialogueContent);
+            //设置任务位置
+            SetCharacterPos(scriptDatas[scriptIndex].characterPos);
         }
     }
     //设置背景图片
     private void SetBGImageSprite(string spriteName)
     {
-
+        UIManager.Instance.SetBGImgSprite(spriteName);
     }
     public void LoadNextScript()
     {
@@ -69,16 +80,21 @@ public class GameManager : MonoBehaviour
     //显示人物
     private void ShowCharacter(string name)
     {
-
+        UIManager.Instance.ShowCharacter(name);
     }
     //更新对话框
     private void UpdateTalkLineText(string dialogueContent)
     {
+        UIManager.Instance.UpdateTalkLineText(dialogueContent);
+    }
 
+    public void SetCharacterPos(int posID)
+    {
+        UIManager.Instance.SetCharacterPos(posID);
     }
 }
 /// <summary>
-/// 剧本数据
+/// 剧本数据类
 /// </summary>
 public class ScriptData
 {
@@ -86,4 +102,6 @@ public class ScriptData
     public string name;//角色名称
     public string spriteName;//资源
     public string dialogueContent;//对话内容
+    public int characterPos;//1.左 2.右 3.中
+    public bool ifRotate;
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,67 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
+
+    public static UIManager Instance { get; private set; }
+
+    public Image imgBG;
+    public Image imgCharacter;
+    public Text textName;
+    public Text textTalkline;
+    public GameObject talkLineGo;//对话框父对象
+    public Transform[] characterPosTrans;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    /// <summary>
+    /// 设置背景图片
+    /// </summary>
+    /// <param name="spriteName"></param>
+    public void SetBGImgSprite(string spriteName)
+    {
+        imgBG.sprite = Resources.Load<Sprite>("Sprites/BG/"+spriteName);
+    }
+    /// <summary>
+    /// 显示人物
+    /// </summary>
+    /// <param name="name"></param>
+    public void ShowCharacter(string name)
+    {
+        talkLineGo.SetActive(true);
+        imgCharacter.sprite = Resources.Load<Sprite>("Sprites/Characters/" + name);
+        imgCharacter.gameObject.SetActive(true);
+        textName.text = name;
+    }
+    /// <summary>
+    /// 更新对话内容
+    /// </summary>
+    /// <param name="dialogueContent"></param>
+    public void UpdateTalkLineText(string dialogueContent)
+    {
+        textTalkline.text = dialogueContent;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="posID"></param>
+    public void SetCharacterPos(int posID,bool ifRotate = false)
+    {
+        imgCharacter.transform.localPosition = characterPosTrans[posID-1].localPosition;
     }
 }
